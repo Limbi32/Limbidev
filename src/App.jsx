7a8982fef8d06ed1b710cb986a17cc385dbcbbf0
unknown <bondmandev@gmail.com>
2024-07-27 19:52:53 +0300

    changement 205

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
 import './App.css'
 import Profil from './assets/font1.jpg'
+import iconmenu from './assets/icon-menu.svg'
+import iconmenuclose from './assets/icon-menu-close.svg'
 import js from './assets/js.jpg'
 import git from './assets/git.jpg'
 import jquery from './assets/jquery.jpg'
@@ -16,7 +18,7 @@ import galerie from './assets/galerie.jpg'
 import pdp from './assets/pdp.jpg'
 import Input from './Components/Form/Input'
 import Textarea from './Components/Form/Textarea'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 
 
@@ -27,7 +29,54 @@ function App() {
     const [value,setValue]=useState('')
     const [value1,setValue1]=useState('')
     const [taille, setTalle]=useState(window.innerWidth)
+    const [burgeractive,setBurgeractive]=useState(true)
+    const  observer1=new IntersectionObserver((entries)=>{
+    
+     
+      for (const entry of entries) {
    
+        if (entry.isIntersecting) {
+          entry.target.animate([ 
+            {transform:'translateX(100px)',opacity:0},
+            {transform:'translateX(50px)',opacity:1}
+          ],{
+            duration: 500
+          })
+        }
+      }
+    })
+
+    const  observer2=new IntersectionObserver((entries)=>{
+    
+     
+      for (const entry of entries) {
+   
+        if (entry.isIntersecting) {
+          entry.target.animate([ 
+            {transform:'translateY(10px)',opacity:0},
+            {transform:'translateY(0)',opacity:1}
+          ],{
+            duration: 500
+          })
+        }
+      }
+    })
+    const ref2=useRef(null)
+    const refskills=useRef(null)
+    const refprojet1=useRef(null)
+    const refprojet2=useRef(null)
+    const refprojet3=useRef(null)
+    const profilref=useRef(null)
+    const refburger=useRef(null)
+    const handlermenu=()=>{
+      setBurgeractive(!burgeractive)
+      if (burgeractive===false) {
+        refburger.current.classList.remove('active')
+      }else{
+        refburger.current.classList.add('active')
+      }
+      console.log(refburger.current.classList);
+    }
    const changetaille=()=>{
     setTalle(window.innerWidth)
    }
@@ -40,8 +89,15 @@ function App() {
     const handleChange1=(e)=>{
       setValue1(e.target.value)
     }
+   
 
     useEffect(()=>{
+      observer1.observe(ref2.current)
+      observer1.observe(profilref.current)
+      observer1.observe(refprojet1.current)
+      observer1.observe(refprojet2.current)
+      observer1.observe(refprojet3.current)
+      observer2.observe(refskills.current)
       window.addEventListener('resize',changetaille)
     },[])
   return (
@@ -55,20 +111,24 @@ function App() {
        
         <nav>
         
-          <ul>
-            <li><a href="">Accueil</a></li>
-            <li><a href="">A propos</a></li>
-            <li><a href="">Projets</a></li>
-            <li><a href="">Contact</a></li>
-         
-          </ul>
+                <ul ref={refburger} className=''>
+             <li><a href="">Accueil</a></li>
+             <li><a href="">A propos</a></li>
+             <li><a href="">Projets</a></li>
+             <li><a href="">Contact</a></li>
+          
+           </ul>
+           {taille<900 && <button onClick={handlermenu} className="menu">
+          { burgeractive ? <img src={iconmenu} alt="" />:<img src={iconmenuclose} alt="" />}
+          </button> 
+           } 
         </nav>
       
       </header>
       <div className="corps">
         
         <div className="presentation">
-        <img src={Profil} alt="" />
+        <img  src={Profil} alt="" />
             <div className="slide">
             <h1>Developpeur Full-stack</h1>
             <p>LIMBIARISAONA François d'assise</p>
@@ -76,12 +136,12 @@ function App() {
             {/* <button>Download CV</button> */}
         </div>
         <div className="about">
-              <h3>About</h3>
+              <h3>A Propos</h3>
               <div className="desc">
-                <div className="img">
+                <div ref={profilref} className="img">
                     <img src={pdp} alt="" />
                 </div>
-                <div className="renseignement">
+                <div  ref={ref2} className="renseignement">
                   Je m'appelle LIMBIARISAONA François d'Assise, developpeur web full-stack, disposant un diplome
                   de MASTER en informatique. J'habite a madagascar, j'ai etudié à l'ecole normale a antsiranana.
                   je suis de nationalite malagasy. En ce moment, je travaille en tant que developpeur web 
@@ -95,7 +155,7 @@ function App() {
         <div className="projects">
             <h3>Mes réalisations</h3>
             <div className="list-projects">
-              <div className="detail-project">
+              <div ref={refprojet1} className="detail-project">
                 <img src={homepage} alt="" />
                 <ul className="techno">
                     <li>React</li>
@@ -104,7 +164,7 @@ function App() {
                <p>Homepage</p><a href="">Visiter</a>
                </div>
               </div>
-              <div className="detail-project">
+              <div ref={refprojet2} className="detail-project">
                 <img src={dessert} alt="https://homepage01.vercel.app/" />
                   <ul className="techno">
                     <li>React</li>
@@ -114,7 +174,7 @@ function App() {
                 <p>Produit-List</p><a href="https://liste-produit.vercel.app/">Visiter</a>
                 </div>
               </div>
-              <div className="detail-project">
+              <div ref={refprojet3} className="detail-project">
                 <img src={galerie} alt="" />
                   <ul className="techno">
                     <li>Html</li>
@@ -130,7 +190,7 @@ function App() {
         </div>
         <div className="skills">
             <h3>Mes compétences</h3>
-            <div className="list-skills">
+            <div ref={refskills} className="list-skills">
              
              
               <div className="desc-skills">
